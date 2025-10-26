d66574b0a65ccdbe1cbebfe8e8e50abb0db37d51
Yanai Yakuel <yanai.yakuel@gmail.com>
2025-10-26 19:36:52 +0200

    Create AprilTagLimelightTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+public class AprilTagLimelightTest extends OpMode {
+    private Limelight3A limelight;
+    private IMU imu;
+
+    // This code makes the robot use its camera to see AprilTags
+    // and figure out where it is and which way it’s facing. It shows that information on the screen.
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(8); // we need to declare pipeline -  April tag = 8
+        imu = hardwareMap.get(IMU.class,"imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+    }
+
+    @Override
+    public void start() {
+        limelight.start();
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()){
+            Pose3D botpose = llResult.getBotpose_MT2();
+            telemetry.addData("target_x", llResult.getTx());
+            telemetry.addData("target_Y", llResult.getTy());
+            telemetry.addData("target_Area", llResult.getTa());
+            telemetry.addData("Yaw", botpose.getOrientation().getYaw());
+        }
+    }
+}
